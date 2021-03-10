d3b1eb9fa8065b8feae4a818602dcc95a3274c6c
DevinBurts <devinburts0@gmail.com>
2021-03-10 14:37:14 -0600

    Milestone 1

---
+++ GPE-340 Project/Assets/Scripts/InputController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private Animator characterAnimator;
+    [SerializeField]
+    private KeyCode jump;
+    [SerializeField]
+    private KeyCode forwards;
+    [SerializeField]
+    private KeyCode backwards;
+    [SerializeField]
+    private KeyCode left;
+    [SerializeField]
+    private KeyCode right;
+    public PlayerPawn playerPawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gain access to the animation controller
+        characterAnimator = GetComponent<Animator>();
+        playerPawn = GetComponent<PlayerPawn>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // play the running animation that corresponds with the player's input
+        
+        if (Input.GetKey(forwards) || Input.GetKey(backwards))
+        {
+            playerPawn.Walk();
+        }
+        if (Input.GetKey(left) || Input.GetKey(right))
+        {
+            playerPawn.Strafe();
+        }
+        if (Input.GetKeyUp(forwards) && Input.GetKeyUp( backwards) && Input.GetKeyUp(left) && Input.GetKeyUp(right))
+        {
+            // Enter the Idle animation when no keys are being pressed
+            playerPawn.Idle();
+        }
+
+        playerPawn.RotateTowards(GetMousePosition());
+
+        // play the jump animation when the jump key is pressed
+        if (Input.GetKeyDown(jump))
+        {
+            playerPawn.Jump();
+
+        }
+    }
+
+    public Vector3 GetMousePosition()
+    {
+        // Variable to store the position of the mouse 
+        Vector3 mousePointInWorld = new Vector3();
+           // Create a plane based on the player's position
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        // Generate a ray to point to the mouse's position
+        Ray mouseRay;
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDistance;
+        // Check if the player's plane and the mouse ray intersect
+        if (playerPlane.Raycast(mouseRay, out hitDistance))
+        {
+            mousePointInWorld = mouseRay.GetPoint(hitDistance);
+        }
+        return mousePointInWorld;
+    }
+}
---
+++ GPE-340 Project/Assets/Scripts/PlayerPawn.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPawn : Pawn
+{
+    private Animator characterAnimator;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float jumpForce;
+    private Quaternion targetRotation;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gain access to the animation controller
+        characterAnimator = GetComponent<Animator>();
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Idle()
+    {
+        // Enter the idle animation
+        characterAnimator.SetFloat("Forward", 0);
+        characterAnimator.SetFloat("Right", 0);
+    }
+
+    public void Walk()
+    {
+        // normalize movement vector
+        moveDirection.Normalize();
+
+        //Change the movedirection to be based on world space
+        moveDirection = transform.InverseTransformDirection(moveDirection);
+        moveDirection.z = Input.GetAxis("Vertical");
+        // Walk forwards and backwards
+        characterAnimator.SetFloat("Forward", moveDirection.z);
+
+    }
+
+    public void Jump()
+    {
+        // Enter the Jump animation and elevate the player
+        characterAnimator.SetBool("Jumping", true);
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up, ForceMode.Impulse);
+        RaycastHit hit;
+        // Check if the player is grounded
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f))
+        {
+            characterAnimator.SetBool("Airborne", true);
+        }
+        else
+        {
+            characterAnimator.SetBool("Airborne", false);
+            characterAnimator.SetBool("Jumping", false);
+        }
+    }
+
+    public void Strafe()
+    {
+        // normalize movement vector
+        moveDirection.Normalize();
+
+        //Change the movedirection to be based on world space
+        moveDirection = transform.InverseTransformDirection(moveDirection);
+        moveDirection.x = Input.GetAxis("Horizontal");
+        // Move to the left and right
+        characterAnimator.SetFloat("Right", moveDirection.x);
+    }
+    public void RotateTowards(Vector3 targetPoint)
+    {
+        // Find the distance between the target and the player's posiiton
+        Vector3 vectorToTarget = targetPoint - transform.position;
+        // Create a rotation 
+        targetRotation = Quaternion.LookRotation(vectorToTarget, Vector3.up);
+
+        // rotate towards a target per frame
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+        //transform.position.RotateTowards(targetPoint);
+    }
+}
